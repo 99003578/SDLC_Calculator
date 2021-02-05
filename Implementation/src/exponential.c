9a861632a596c5f0b6900f9d3c1246da17eea3c2
Karthik Manoj <30859340+Karthik-Manoj@users.noreply.github.com>
2021-02-06 04:12:59 +0530

    ll

---
+++ Implementation/src/exponential.c
@@ -0,0 +1,78 @@
+#include <exponential.h>
+#include <math.h>
+#include <stdio.h>
+
+int exponential()
+{
+    float basenumber;
+    int exponential, n;
+    float result = 1;
+    
+    printf("Enter the Base number : ");
+    scanf("%f", &basenumber);
+    if(basenumber == 0)
+    {
+        printf("Invalid Operand\n");
+        printf("Enter a valid Base number : ", &basenumber);
+        scanf("%f", &basenumber);
+    }
+    
+    printf("Enter the Exponential : ");
+    scanf("%d", &exponential);
+
+    while (exponential != 0)
+    {
+        result *= basenumber;
+        --exponential;
+    }
+    printf("If you want to print the result press 1 or else press 2 : ");
+    scanf("%d", &n);
+    if (n == 1){
+        printf("Solution = %.02lf", result);
+        return 0;
+    }
+
+    else{
+        return result;// Integreators place to be added
+
+    }
+    
+}
+
+int exponential1(float x, int y)
+{
+    float basenumber = x;
+    int exponential = y;
+    int n;
+    float result = 1;
+    
+    printf("Enter the Base number : ");
+    
+    if(basenumber == 0)
+    {
+        printf("Invalid Operand\n");
+        printf("Enter a valid Base number : ", &basenumber);
+        scanf("%f", &basenumber);
+    }
+    
+    printf("Enter the Exponential : ");
+    
+
+    while (exponential != 0)
+    {
+        result *= basenumber;
+        --exponential;
+    }
+    printf("If you want to print the result press 1 or else press 2 : ");
+    scanf("%d", &n);
+    if (n == 1){
+        printf("Solution = %.02lf", result);
+        return 0;
+    }
+
+    else{
+        return result;// Integreators place to be added
+
+    }
+    
+}

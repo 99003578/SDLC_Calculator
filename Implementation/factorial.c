5b8a1bc25da288a578d0486b768ce97555018243
stepin104312 <69116163+stepin104312@users.noreply.github.com>
2021-02-05 18:28:09 +0530

    rr

---
+++ Implementation/factorial.c
@@ -0,0 +1,50 @@
+#include<factorial.h>
+int factorial()
+{   
+signed int n, factorial;
+printf("enter the number");
+scanf("%d",&n);
+factorial=fact(n);
+printf("enter 1 for continuation else 2");
+int k;
+scanf("%d", &k);
+if(k=1){
+    return factorial;
+}
+else{
+    printf("factorial of %d = %d",n,factorial);
+    return 0;
+}
+
+int factorial2(signed int x){
+int n1;
+    n1 = x;
+    factorial=fact(n1);
+    printf("enter 1 for continuation else 2");
+int k;
+scanf("%d", &k);
+if(k=1){
+    return factorial;
+}
+else{
+    printf("factorial of %d = %d",n,factorial);
+    return 0;
+}
+
+}
+
+int fact(int n)
+{ 
+    if(n<0)
+  printf("error! Factorial of negative number doesn't exist");
+  else {
+    int i,f=1;
+
+    for (i=1;i<=n;i++)
+     {
+    f=f*i;
+      }
+ return f;
+  }  
+}
+
---
+++ Implementation/factorial.h
@@ -0,0 +1,7 @@
+#ifndef __FACTORIAL_H__
+#define __FACTORIAL_H__
+
+#include<stdio.h>
+ int factorial();
+ int factorial2(signed int x);
+#endif 

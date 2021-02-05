616624fe5a44f1389a296c398a7d9144f34328b8
Karthik Manoj <30859340+Karthik-Manoj@users.noreply.github.com>
2021-02-06 05:08:12 +0530

    aa

--- Implementation/main.c
+++ Implementation/main.c
@@ -12,7 +12,8 @@
 int main(){
     int k, j=3;
     int r;
-    while (j != 0){
+    int r2;
+   
         printf("Please select the number for the select function \n");
         printf("1 - basic operators(Add, Subtract, multiply, division)\n");
         printf("2 - average\n");
@@ -28,39 +29,159 @@ int main(){
         switch(k){
             case 1:
             r = basic_operations();
+            if(r==0){
+                break;
+            }
+            else{
+                goto front;
+            }
+            case 2:
+            r = average();
+            if(r==0){
+                break;
+            }
+            else{
+                goto front;
+            }
+            case 3:
+            r = bin_to_dec();
+           if(r==0){
+                break;
+            }
+            else{
+                goto front;
+            }
+            case 4:
+            r = dec_to_bin();
+            if(r==0){
+                break;
+            }
+            else{
+                goto front;
+            }
+            case 5:
+            r = exponential();
+            if(r==0){
+                break;
+            }
+            else{
+                goto front;
+            }
+            case 6:
+            r = factorial();
+           if(r==0){
+                break;
+            }
+            else{
+                goto front;
+            }
+            case 7:
+            r = interest();
+            if(r==0){
+                break;
+            }
+            else{
+                goto front;
+            }
+            case 8:
+            r = logarthmic();
+            if(r==0){
+                break;
+            }
+            else{
+                goto front;
+            }
+            case 9:
+            r = power();
+           if(r==0){
+                break;
+            }
+            else{
+                goto front;
+            }
+            case 10:
+            trignometry();
+            break;
+
+     }
+
+    front: 
+        printf("Please select the number for the select function \n");
+        printf("1 - basic operators(Add, Subtract, multiply, division)\n");
+        printf("2 - average\n");
+        printf("3 - Binary to Decimal conversion\n");
+        printf("4 - Decimal to Binary conversion\n");
+        printf("5 - Exponential operation\n");
+        printf("6 - Factorial operation\n");
+        printf("7 - Simple Interest Operation\n");
+        printf("8 - Logarthmic Operation\n");
+        printf("9 - Taking Power of the number");
+
+         scanf("%d", &k);
+        switch(k){
+            case 1:
+            printf("enter the second number");
+            scanf("%d", &r2)
+            r = basic_operations1(r, r2);
+            if(r==0){
+                break;
+            }
+            else{
+                goto front;
+            }
+            case 2:
+            r = average1(r);
+            if(r==0){
+                break;
+            }
+            else{
+                goto front;
+            }
+            case 5:
+            printf("Enter the exponential:");
+            scanf("%d", &r2);
+            r = exponential1(r, r2);
+            if(r==0){
+                break;
+            }
+            else{
+                goto front;
+            }
+            case 6:
+            r = factorial();
+           if(r>0){
+                break;
+            }
+            else{
+                goto front;
+            }
+            case 7:
+            r = interest();
             if(r>0){
                 break;
             }
             else{
-                goto : front
-            case 2:
-            r = average();
-            break;
-            case 3:
-            r = bin_to_dec();
-            break;
-            case 4:
-            r = dec_to_bin();
-            break;
-            case 5:
-            r = exponential();
-            break;
-            case 6:
-            r = factorial();
-            break;
-            case 7:
-            r = interest();
-            break;
+                goto front;
+            }
             case 8:
             r = logarthmic();
-            break;
+            if(r>0){
+                break;
+            }
+            else{
+                goto front;
+            }
             case 9:
             r = power();
-            break;
-            case 10:
-            r = trignometry();
-            break;
+           if(r>0){
+                break;
+            }
+            else{
+                goto front;
+            }
 
-     }
+
+
+
+
     }
-}
--- Implementation/src/basic_operations.c
+++ Implementation/src/basic_operations.c
@@ -45,3 +45,44 @@ int basic_operations(){
         return 0;
     }
 }
+
+
+int basic_operation1(int x, int y){
+    float k, j, l;
+    x = j;
+    y = l;
+    int m;
+    printf("Choose your operation\n");
+    printf("1 - Addition\n");
+    printf("2 - Subtraction\n");
+    printf("3 - Multipilcation\n");
+    printf("4 - Division\n");
+    scanf("%f", &k);
+
+    switch(k){
+        case 1:
+        int result = j + l;
+        printf("Answer is %.0f", &result);
+        break;
+        case 2:
+        int result = j - l;
+        printf("Answer is %.0f", &result);
+        break;
+        case 3:
+        int result = j * l;
+        printf("Answer is %.0f", &result);
+        break;
+        case 4:
+        int result = j / l;
+        printf("Answer is %.0f", &result);
+        break;
+    }
+    printf("Enter 1 if you want to continue else 2");
+    scanf("%d", &m);
+    if(m == 1){
+        return result;
+    }
+    if(m == 2){
+        return 0;
+    }
+}

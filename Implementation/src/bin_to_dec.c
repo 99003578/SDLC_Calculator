b8bcadb33f0f436315c9c33587cae24be185886c
99003513 <78538094+99003513@users.noreply.github.com>
2021-02-05 20:27:35 +0530

    binary to decimal

---
+++ Implementation/src/bin_to_dec.c
@@ -0,0 +1,34 @@
+#include<bin_to_dec.h>
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+    int binnum, decnum=0, i=0, rem,m;
+    printf("Enter any binary number: ");
+    scanf("%d", &binnum);
+    if(num<0 && num>1)
+    {
+        printf("invalid input");
+    }
+    else
+    {
+        while(binnum!=0)
+        {
+            rem = binnum%10;
+            decnum = decnum + rem*pow(2,i);
+            i++;
+            binnum = binnum/10;
+        }
+    }
+    printf("To print result press 1 else press 2 ");
+    scanf("%d",&m);
+    if(m==1)
+    {
+    printf("\nEquivalent Decimal Value = %d", decnum);
+    }
+    else{
+        return I;
+    }
+    return 0;
+}

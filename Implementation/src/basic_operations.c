80b8c0f23e13ee7b8be86270b459bdc16499dc6b
Karthik Manoj <30859340+Karthik-Manoj@users.noreply.github.com>
2021-02-05 22:39:58 +0530

    Update basic_operations.c

--- Implementation/src/basic_operations.c
+++ Implementation/src/basic_operations.c
@@ -1,21 +1,47 @@
 #include<basic_operations.h>
 
 int basic_operations(){
-    int k, j, l, m;
+    float k, j, l;
+    int m;
     printf("Choose your operation\n");
     printf("1 - Addition\n");
     printf("2 - Subtraction\n");
     printf("3 - Multipilcation\n");
     printf("4 - Division\n");
-    scanf("%d", &k);
+    scanf("%f", &k);
 
     switch(k){
         case 1:
         printf("Please enter the numbers\n");
         scanf("%d %d", &j, &l);
         int result = j + l;
-        
-
-
+        printf("Answer is %.0f", &result);
+        break;
+        case 2:
+        printf("Please enter the numbers\n");
+        scanf("%d %d", &j, &l);
+        int result = j - l;
+        printf("Answer is %.0f", &result);
+        break;
+        case 3:
+        printf("Please enter the numbers\n");
+        scanf("%d %d", &j, &l);
+        int result = j * l;
+        printf("Answer is %.0f", &result);
+        break;
+        case 4:
+        printf("Please enter the numbers\n");
+        scanf("%d %d", &j, &l);
+        int result = j / l;
+        printf("Answer is %.0f", &result);
+        break;
+    }
+    printf("Enter 1 if you want to continue else 2");
+    scanf("%d", &m);
+    if(m == 1){
+        return result;
+    }
+    if(m == 2){
+        return 0;
     }
 }

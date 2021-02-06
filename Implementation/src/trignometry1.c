3df791a89453e6223b9278ce7e71434273c5f030
99003507 <78578114+99003507@users.noreply.github.com>
2021-02-06 15:40:26 +0530

    Update trignometry1.c

--- Implementation/src/trignometry1.c
+++ Implementation/src/trignometry1.c
@@ -23,16 +23,25 @@ int trignometry1()
 }   
 int trig(int angle)  
 {  
-    float degree, radian;  
+    float angle, radian;  
     const float PI = 3.14159;  
   
-    printf("Enter angle in degree\n");  
-    scanf("%f",&degree);  
+    printf("Enter angle \n");  
+    scanf("%f",&angle);  
   
-    radian = degree * (PI / 180.0);  
+    radian = angle * (PI / 180.0);  
   
-    printf("Sin(%f) = %f\n", degree, sin(radian));  
-    printf("Cos(%f) = %f\n", degree, cos(radian));  
-    printf("Tan(%f) = %f\n", degree, tan(radian)); 
+    printf("Sin(%f) = %f\n", angle, sin(radian));  
+    printf("Cos(%f) = %f\n", angle, cos(radian));  
+    printf("Tan(%f) = %f\n", angle, tan(radian)); 
     return 0;
+}
+int trig_test(int x)
+{
+  float angle,radian; 
+ const float PI = 3.14159;  
+  x = angle;
+ 
+ return radian = angle * (PI / 180.0);  
+
 }

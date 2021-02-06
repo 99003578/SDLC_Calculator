8acd942817430f1257dbe85ef978b5a5070145b7
Karthik Manoj <30859340+Karthik-Manoj@users.noreply.github.com>
2021-02-06 06:51:39 +0530

    AA

--- Implementation/Test/test_calculator.c
+++ Implementation/Test/test_calculator.c
@@ -1,5 +1,5 @@
 #include<average_operations.h>
-#include<basic_operations.h>
+#include<basic_operation.h>
 #include<bin_to_dec.h>
 #include<dec_to_bin.h>
 #include<exponential.h>
@@ -7,8 +7,25 @@
 #include<interest.h>
 #include<logarthmic.h>
 #include<power.h>
-#include<trignometry.h>
+#include<trignometry1.h>
 #include "unity.h"
 
 #define PROJECT_NAME "SDLC_Calculator"
+
+void test_add_1(void);
+void test_add_2(void);
+void test_subtract_1(void);
+void test_subtract_2(void);
+void test_multiply(void);
+void test_divide_1(void);
+void test_divide_2(void);
+
+void setUp(){}
+
+void tearDown(){}
+
+int main(){
+    UNITY_BEGIN();
+}
+
 

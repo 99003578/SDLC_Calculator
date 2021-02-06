9c3f0c52330efb3d1a08f53c7c617c9b8059f42e
99003514 <78578085+99003514@users.noreply.github.com>
2021-02-06 23:12:42 +0530

    Update basic_operation.c

--- Implementation/src/basic_operation.c
+++ Implementation/src/basic_operation.c
@@ -30,7 +30,7 @@ int basic_operation(){
         printf("Please enter the numbers\n");
         scanf("%f %f", &j, &l);
      result = j - l;
-        printf("Answer is %d", &result);
+        printf("Answer is %d", result);
         printf("Enter 1 if you want to continue else 2");
     scanf("%d", &m);
     if(m == 1){
@@ -44,7 +44,7 @@ int basic_operation(){
         printf("Please enter the numbers\n");
         scanf("%f %f", &j, &l);
      result = j * l;
-        printf("Answer is %d", &result);
+        printf("Answer is %d", result);
         printf("Enter 1 if you want to continue else 2");
     scanf("%d", &m);
     if(m == 1){
@@ -65,7 +65,7 @@ int basic_operation(){
         printf("Please enter the numbers\n");
         scanf("%f %f", &j, &l);
         result = j / l;
-        printf("Answer is %d", &result);
+        printf("Answer is %d",result);
         printf("Enter 1 if you want to continue else 2");
     scanf("%d", &m);
     if(m == 1){
@@ -98,7 +98,7 @@ int basic_operation1(int x, int y){
         int result;
         case 1:
         result = j + l;
-        printf("Answer is %d", &result);
+        printf("Answer is %d",result);
         printf("Enter 1 if you want to continue else 2");
     scanf("%d", &m);
     if(m == 1){
@@ -110,7 +110,7 @@ int basic_operation1(int x, int y){
         break;
         case 2:
         result = j - l;
-        printf("Answer is %d", &result);
+        printf("Answer is %d", result);
         printf("Enter 1 if you want to continue else 2");
     scanf("%d", &m);
     if(m == 1){
@@ -122,7 +122,7 @@ int basic_operation1(int x, int y){
         break;
         case 3:
         result = j * l;
-        printf("Answer is %d", &result);
+        printf("Answer is %d", result);
         printf("Enter 1 if you want to continue else 2");
     scanf("%d", &m);
     if(m == 1){
@@ -141,7 +141,7 @@ int basic_operation1(int x, int y){
         }
         else{
         result = j / l;
-        printf("Answer is %f", &result);
+        printf("Answer is %d", result);
         printf("Enter 1 if you want to continue else 2");
     scanf("%d", &m);
     if(m == 1){

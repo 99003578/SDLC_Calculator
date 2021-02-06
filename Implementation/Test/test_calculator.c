6278829c4d5305f88541eb5f3686e7e1e01476af
99003514 <69049989+stepin104991@users.noreply.github.com>
2021-02-06 12:17:35 +0530

    Update test_calculator.c

--- Implementation/Test/test_calculator.c
+++ Implementation/Test/test_calculator.c
@@ -34,43 +34,65 @@ int main(){
     RUN_TEST(test_multiply);
     RUN_TEST(test_divide_1);
     RUN_TEST(test_divide_2);
+    RUN_TEST(test_factorial(void)
 
     return UNITY_END();
 }
 
 
 void test_add_1(void){
-    TEST_ASSERT_EQUAL(25, basic_operation_test(20, 5, 1));
+    TEST_ASSERT_EQUAL(25, add1(20, 5, 1));
 }
 
 void test_add_2(void){
 <<<<<<< Updated upstream
-    TEST_ASSERT_EQUAL(-10, basic_operaion_test(-30, 10, 1));
+    TEST_ASSERT_EQUAL(-10, add2(-30, 10, 1));
 =======
-    TEST_ASSERT_EQUAL(-20, basic_operaion_test(-30, 10, 1);)
+    TEST_ASSERT_EQUAL(-20, add2(-30, 10, 1);)
 >>>>>>> Stashed changes
 }
 
 void test_subtract_1(void){
-    TEST_ASSERT_EQUAL(15, basic_operation_test(20, 5, 2));
-    TEST_ASSERT_EQUAL(-3, basic_operation_test(2, 5, 2));
-}
+    TEST_ASSERT_EQUAL(15, subtract1(20, 5, 2));
+    TEST_ASSERT_EQUAL(-3, subtract1(2, 5, 2));
 }
 
 void test_subtract_2(void){
-    TEST_ASSERT_EQUAL(-40, basic_operaion_test(-30, 10, 2));
-    TEST_ASSERT_EQUAL(-8, basic_operaion_test(-2, 6, 2));
+    TEST_ASSERT_EQUAL(-40, subtract2(-30, 10, 2));
+    TEST_ASSERT_EQUAL(-8, subtract2(-2, 6, 2));
 }
 void test_multiply_1(void){
-    TEST_ASSERT_EQUAL(6, basic_operation_test(2, 3, 3));
+    TEST_ASSERT_EQUAL(6, multiply1(2, 3, 3));
 }
 
 void test_multiply_2(void){
-    TEST_ASSERT_EQUAL(-18, basic_operaion_test(-3, 6, 3));
+    TEST_ASSERT_EQUAL(-18, multiply2(-3, 6, 4));
 }
 
 void test_divide_1(void){
-    TEST_ASSERT_EQUAL(2, basic_operaion_test(6, 2, 4));
+    TEST_ASSERT_EQUAL(2, divide(6, 2, 4));
+}
+
+void test_factorial(void){
+    TEST_ASSERT_EQUAL(120, factorial(5));
+}
+
+void test_factorial(void){
+    TEST_ASSERT_EQUAL(1, factorial(0));
+}
+
+void test_factorial(void){
+TEST_ASSERT_EQUAL("error! Factorial of negative number doesn't exist", factorial(-10));
+}
+
+void test_logarithmic(void){
+    TEST_ASSERT_EQUAL(3, logarithmic(8,2));
+}
+void test_logarithmic(void){
+    TEST_ASSERT_EQUAL("Invalid", logarithmic(-8,2));
+}
+void test_logarithmic(void){
+    TEST_ASSERT_EQUAL("Invalid", logarithmic(8,-2));
 }
 
 
@@ -151,59 +173,6 @@ void test_divide_1(void){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-void test_interest_1(void){
-    TEST_ASSERT_EQUAL(2250, basic_operation_test(10000, 3, 7.5));
-    TEST_ASSERT_EQUAL(5100, basic_operation_test(15000, 5, 6.8));
 }
 
-void test_interest_2(void){
-    TEST_ASSERT_EQUAL(1650, basic_operaion_test(3000, 10, 5.5));
-    TEST_ASSERT_EQUAL(780, basic_operaion_test(5000, 2, 7.8));
-}
+
